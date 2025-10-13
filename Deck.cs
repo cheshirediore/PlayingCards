3481dcf2eb5823dc657ff92d74b7ecfef8db2e07
Cheshire <cheshirediore@gmail.com>
2025-10-13 17:21:12 -0400

    Implement shuffle method

--- Deck.cs
+++ Deck.cs
@@ -18,14 +18,44 @@ namespace CheshireDiore.PlayingCards
 
         public void AddCard(Card card)
         {
-            Console.WriteLine($"DEBUG> Deck.cs: Created card: {card}");
+            // Console.WriteLine($"DEBUG> Deck.cs: Adding card: {card}");
             _cards.Push(card);
-            Console.WriteLine($"DEBUG> Deck.cs: Deck now contains {_cards.Count} cards.");
-            Console.WriteLine($"DEBUG> Deck.cs: Deck stack now has a capacity of {_cards.Capacity}.");
+            // Console.WriteLine($"DEBUG> Deck.cs: Deck now contains {_cards.Count} cards.");
+            // Console.WriteLine($"DEBUG> Deck.cs: Deck stack now has a capacity of {_cards.Capacity}.");
+        }
+        public Card DrawCard()
+        {
+            return _cards.Pop();
         }
 
         public void Shuffle()
         {
+            Random random = new();
+            // Fisher–Yates shuffle
+            // -- To shuffle an array a of n elements (indices 0..n − 1):
+            // for i from n − 1 down to 1 do
+            //     j ← random integer such that 0 ≤ j ≤ i
+            //     exchange a[j] and a[i]
+            // Make a copy of the stack's backing array
+            Card[] workingCopy = _cards.ToArray();
+            // Start the shuffling
+            for (int i = workingCopy.Length - 1; i >= 0; i--)
+            {
+                // Console.WriteLine($"DEBUG> Deck.cs: i = {i}");
+                var value1 = workingCopy[i];
+                int randomlySelectedIndex = random.Next(i);
+                // Console.WriteLine($"DEBUG> Deck.cs: j = {randomlySelectedIndex}");
+                var value2 = workingCopy[randomlySelectedIndex];
+                // Console.WriteLine($"DEBUG> Deck.cs: Swapping {value1} and {value2}");
+                workingCopy[i] = value2;
+                workingCopy[randomlySelectedIndex] = value1;
+            }
+            _cards.Clear();
+            foreach (Card card in workingCopy)
+            {
+                AddCard(card);
+            }
+
         }
 
         public Deck Deal(int cardCount)
@@ -34,11 +64,11 @@ namespace CheshireDiore.PlayingCards
             for (int i = 0; i < cardCount; i++)
             {
                 // _cards.Pop());
-                Card card = _cards.Pop();
-                Console.WriteLine($"DEBUG> Deck.cs: Dealing {card}...");
+                Card card = DrawCard();
+                // Console.WriteLine($"DEBUG> Deck.cs: Dealing {card}...");
                 dealtCards.AddCard(card);
             }
-            Console.WriteLine($"DEBUG> Deck.cs: Dealt {dealtCards.Count} cards");
+            // Console.WriteLine($"DEBUG> Deck.cs: Dealt {dealtCards.Count} cards");
             return dealtCards;
         }
     }
--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 
 // See https://aka.ms/new-console-template for more information
 
+// Initialize an empty deck
 Deck deck = new();
+// Add the 52 standard playing cards
 for (int suit = 0; suit < 4; suit++)
 {
     for (int rank = 2; rank < 15; rank++)
@@ -11,7 +13,13 @@ for (int suit = 0; suit < 4; suit++)
         deck.AddCard(card);
     }
 }
+// Shuffle the deck
+// TODO: Implement shuffle
+deck.Shuffle();
+
+// Deal a hand of 5 cards from the deck
 Deck hand = deck.Deal(5);
+// Read each card from hand
 foreach (Card card in hand.Cards)
 {
     Console.WriteLine($"DEBUG> Program.cs: {card}");
